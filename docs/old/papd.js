86d04731838ae04b1ebb575eb835a0b85f0774d6
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2023-07-18 20:12:15 +0500

    updated

---
+++ docs/old/papd.js
@@ -0,0 +1,16 @@
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+// function myFunction() {
+//   var element = document.getElementById("newid");
+//   element.classList.add("eventtext1");
+// }
+ // document.getElementById('button1').addEventListener('click',function new(){
+ //   document.getElementById('newid').classList.add('eventtext1');
+ // });
+
+
+ // const updiv=document.querySelector("btn");
+ // add.addEventListener("click",(){
+ //   updiv.addClassList.remove("eventtext1")
+ // })
